2466f4946ee64e3ac6644c09e4d6c393e1698161
郭晨旭 <2269409349@qq.com>
2023-09-01 15:57:27 +0800

    init: 初次提交, 搭建项目目录结构

---
+++ fakenews-common/src/main/java/com/guochenxu/common/exception/CustomException.java
@@ -0,0 +1,16 @@
+package com.guochenxu.common.exception;
+
+import com.guochenxu.model.common.enums.AppHttpCodeEnum;
+
+public class CustomException extends RuntimeException {
+
+    private AppHttpCodeEnum appHttpCodeEnum;
+
+    public CustomException(AppHttpCodeEnum appHttpCodeEnum){
+        this.appHttpCodeEnum = appHttpCodeEnum;
+    }
+
+    public AppHttpCodeEnum getAppHttpCodeEnum() {
+        return appHttpCodeEnum;
+    }
+}

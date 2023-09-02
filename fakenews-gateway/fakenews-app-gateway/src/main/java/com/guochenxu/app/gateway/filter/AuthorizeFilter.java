edb0889653544c34dec04293e671156592c81954
郭晨旭 <2269409349@qq.com>
2023-09-02 16:12:12 +0800

    add: 完成网关权限校验

---
+++ fakenews-gateway/fakenews-app-gateway/src/main/java/com/guochenxu/app/gateway/filter/AuthorizeFilter.java
@@ -0,0 +1,69 @@
+package com.guochenxu.app.gateway.filter;
+
+import com.guochenxu.app.gateway.utils.AppJwtUtil;
+import io.jsonwebtoken.Claims;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @program: fakenews
+ * @description: 认证权限过滤器
+ * @author: 郭晨旭
+ * @create: 2023-09-02 15:33
+ * @version: 1.0
+ **/
+
+@Component
+@Slf4j
+public class AuthorizeFilter implements Ordered, GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        // 判断是否登录
+        ServerHttpRequest request = exchange.getRequest();
+        ServerHttpResponse response = exchange.getResponse();
+        if (request.getURI().getPath().contains("/login")) {
+            return chain.filter(exchange);
+        }
+
+        // 获取token
+        String token = request.getHeaders().getFirst("token");
+        if (StringUtils.isBlank(token)) {
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
+        }
+
+        // 验证token
+        try {
+            Claims claimsBody = AppJwtUtil.getClaimsBody(token);
+            int res = AppJwtUtil.verifyToken(claimsBody);
+            if (res == 1 || res == 2) {
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                return response.setComplete();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    /**
+     * 设置优先级, 值越小, 优先级越高
+     */
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
